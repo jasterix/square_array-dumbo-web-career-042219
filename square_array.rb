8588fd1e0a397b68e720f3b11567c3a75cf02dfc
Jasterix C <loune.calixte@gmail.com>
2019-04-04 17:15:32 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,16 @@
 def square_array(array)
-  # your code here
+#   # your code here
+#   for each item in array do
+#     item ** 2 
+#   end
+# end
+  arr = []
+ # array.each do |item|
+#   item ** 2
+#   end
+# end
+
+  arr = []
+  array.each { |i| arr << i ** 2 }
+  arr
 end
