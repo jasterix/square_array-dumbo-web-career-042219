780efafe2b6508765ce289661308e49b6141b566
Jasterix C <loune.calixte@gmail.com>
2019-04-04 16:43:41 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,10 @@
 def square_array(array)
-  # your code here
-  for each item in array do
-    item ** 2 
+#   # your code here
+#   for each item in array do
+#     item ** 2 
+#   end
+# end
+  basket.each do |apple|
+    puts "Taking out #{apple}"
+    apples_taken_out += 1
   end
-end
